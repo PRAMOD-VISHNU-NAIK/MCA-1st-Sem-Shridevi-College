b18216d75895063d81965e2ded0d6fb77237a5c6
PRAMOD-VISHNU-NAIK <pammunaik99.51solution@gmail.com>
2025-01-03 13:40:58 +0530

    Updated PPT

--- Lab Programs/Pramod/main.c
+++ Lab Programs/Pramod/main.c
@@ -4,8 +4,47 @@
 int main()
 {
 
-    char ch = -100;
+    char op;
+    double a;
+    double b, res;
 
-    printf("%c", ch);
+    printf("Simple Calculator:\n");
+    printf("Choose an operation:\n");
+    printf(" + : Addition\n");
+    printf(" - : Subtraction\n");
+    printf(" * : Multiplication\n");
+    printf(" / : Division\n");
+    printf(" %% : Modulus (for integers only)\n");
+
+
+    printf("Enter the operator: ");
+    scanf("%c", &op);
+
+    printf("Enter the 1st Number: ");
+    scanf("%lf", &a);
+
+    printf("Enter the 2nd Number: ");
+    scanf("%lf", &b);
+
+
+    if(op == '+'){
+
+        res = a + b;
+
+        printf("Sum is: %.2lf", res);
+    }
+    else if( op == '-'){
+        res = a - b;
+
+        printf("Substraction is: %.2lf", res);
+    }
+    else {
+        printf("ENter a Valid Operator");
+    }
+
+
+
+
+
     return 0;
 }
